7bc9ef2056c45eae2ba353d181e2a17cb8dcaad4
afmeirelles <andrefmeirelles@gmail.com>
2017-04-06 16:33:19 -0300

    removed unused test

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('expect.js')
-var subject = require('../src/test')
-
-describe('something',function(){
-    it('should blabla', function(){
-        expect(subject(5)).to.eql(7)
-    })
-})
